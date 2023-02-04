67af25243ca030e6f30da534eeff9118f76ffc3d
Lond Treeforce <londtreeforce@gmail.com>
2023-02-04 17:21:41 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import Navbar from "./Components/Navbar";
+import Banner from "./Components/Banner";
+import Company from "./Components/Company";
+import Collections from "./Components/Collections";
+import News from "./Components/News";
+import Favourite from "./Components/Favourite";
+import Email from "./Components/Email";
+import Foother from "./Components/Foother";
+
+import './App.css';
+
+
+
+function App() {
+  
+  
+  return (
+    <div className="App">
+      <Navbar />
+      <Banner />
+      <Company />
+      <Collections />
+      <News />
+      <Favourite />
+      <Email />
+      <Foother />
+      
+      
+      
+      
+      
+    </div>
+  );
+}
+
+export default App;
+
+
