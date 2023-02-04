2ab2f7330056e9757d4b7528b5fb50d0bd063325
Lond Treeforce <londtreeforce@gmail.com>
2023-02-04 19:40:03 +0300

    Navbar v2

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,25 +1,40 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './css/navbar.css';
 import Logo from '../img/logo.svg';
+import { FaBars, FaTimes } from 'react-icons/fa'
 
 const Navbar = () => {
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsiv-nav");
+  }
+
   return (
-    <div className='container'>
-      <div className='logo'>
-        <img src={Logo} alt="Logo" />
+    <header>
+      <nav ref={navRef}>
+      <div className='container'>
+        <div className='logo'>
+          <img src={Logo} alt="Logo" />
+        </div>
+        <div className='navbar-link'>
+          {/*Warning, it's href down */}
+          <a href="#catalogue">CATALOGUE</a>
+          <a href="#catalogue">FASHION</a>
+          <a href="#catalogue">FAVOURITE</a>
+          <a href="#catalogue">LIFESTYLE</a>
+          <a href="">SIGNUP</a>
+        </div>
+        
+        
+        
+
       </div>
-      <div className='navbar-link'>
-        {/*Warning, it's href down */}
-        <a href="#catalogue">CATALOGUE</a>
-        <a href="#catalogue">FASHION</a>
-        <a href="#catalogue">FAVOURITE</a>
-        <a href="#catalogue">LIFESTYLE</a>
-        <a href="">SIGNUP</a>
-      </div>
-
-
-    </div>
-
+      </nav>
+      
+      
+      
+    </header>
 
 
   );
